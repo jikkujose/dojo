2423a0bdfac24517a1e8c89361bb93b5d3e12d02
amalshehu <amalshehu@gmail.com>
2020-03-05 20:18:16 +0530

    Add navbar

--- 01 - Spaces-iOS/amalshehu/ui/src/App.tsx
+++ 01 - Spaces-iOS/amalshehu/ui/src/App.tsx
@@ -1,23 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
+      <h2>SPACES iOS</h2>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <nav className="App-header-nav">
+          <a className="App-header-item">SPACES</a>
+          <a className="App-header-item">PROTOTYPES</a>
+          <a className="App-header-item">BOARDS</a>
+          <a className="App-header-item">FAVORITES</a>
+        </nav>
       </header>
     </div>
   );
