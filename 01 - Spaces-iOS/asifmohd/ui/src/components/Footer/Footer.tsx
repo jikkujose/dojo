4686a82216c377d7a004125c6aea831fcbd25775
Asif Mohammed <asifm2882@gmail.com>
2020-03-06 02:02:19 +0530

    UI: add Footer component

--- 01 - Spaces-iOS/asifmohd/ui/src/components/Footer/Footer.tsx
+++ 01 - Spaces-iOS/asifmohd/ui/src/components/Footer/Footer.tsx
@@ -1,6 +1,19 @@
 import React from 'react';
-
+import Diamonds from '../../icons/Diamonds';
+import './Footer.scss';
+import Folder from '../../icons/Folder';
+import Inbox from '../../icons/Inbox';
+import User from '../../icons/User';
 const Footer = () => {
-  return <footer>Header</footer>;
+  return (
+    <footer>
+      <div className="Footer-contianer">
+        <Diamonds />
+        <Folder />
+        <Inbox />
+        <User />
+      </div>
+    </footer>
+  );
 };
 export default Footer;
