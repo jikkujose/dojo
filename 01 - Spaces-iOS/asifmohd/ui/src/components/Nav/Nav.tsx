b4432cf622a52360c9b970a377efaf3910ae7f95
Asif Mohammed <asifm2882@gmail.com>
2020-03-05 21:53:20 +0530

    UI: add Nav component

--- 01 - Spaces-iOS/asifmohd/ui/src/components/Nav/Nav.tsx
+++ 01 - Spaces-iOS/asifmohd/ui/src/components/Nav/Nav.tsx
@@ -1,6 +1,31 @@
 import React from 'react';
+import './Nav.scss';
+import { navData } from './../../model/navdata';
+import search from '../../search.svg';
 
 const Nav = () => {
-  return <nav>Header</nav>;
+  return (
+    <nav>
+      <ul className="Nav-container">
+        {navData.map(n => (
+          <li
+            className={`Nav-list ${n.title === 'SPACES' ? 'Nav-active' : ''}`}
+          >
+            <a
+              className={`Nav-item ${
+                n.title === 'SPACES' ? 'Nav-active-text' : ''
+              }`}
+              href={n.href}
+            >
+              {n.title}
+            </a>
+          </li>
+        ))}
+        <li className="Nav-list">
+          <img src={search} />
+        </li>
+      </ul>
+    </nav>
+  );
 };
 export default Nav;
