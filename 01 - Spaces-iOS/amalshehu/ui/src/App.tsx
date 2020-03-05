7973f12dc20580f655dc0f9affe24a690cd2540b
amalshehu <amalshehu@gmail.com>
2020-03-05 22:27:45 +0530

    Add Navbar component

--- 01 - Spaces-iOS/amalshehu/ui/src/App.tsx
+++ 01 - Spaces-iOS/amalshehu/ui/src/App.tsx
@@ -1,18 +1,16 @@
 import React from 'react';
 import './App.scss';
+import Navbar from '../src/components/Navbar';
 
 function App() {
   return (
     <div className="App">
       <h2>SPACES iOS</h2>
       <header className="App-header">
-        <nav className="App-header-nav">
-          <a className="App-header-item">SPACES</a>
-          <a className="App-header-item">PROTOTYPES</a>
-          <a className="App-header-item">BOARDS</a>
-          <a className="App-header-item">FAVORITES</a>
-        </nav>
+        <Navbar></Navbar>
       </header>
+      <main>Hello world</main>
+      <footer>Footer is coming</footer>
     </div>
   );
 }
---
+++ 01 - Spaces-iOS/amalshehu/ui/src/components/Navbar.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Navbar.scss';
+
+function Navbar() {
+  return (
+    <nav className="App-header-nav">
+      <a href="#" className="App-header-item">
+        SPACES
+      </a>
+
+      <a href="#" className="App-header-item">
+        PROTOTYPES
+      </a>
+      <a href="#" className="App-header-item">
+        BOARDS
+      </a>
+      <a href="#" className="App-header-item">
+        FAVORITES
+      </a>
+    </nav>
+  );
+}
+
+export default Navbar;
