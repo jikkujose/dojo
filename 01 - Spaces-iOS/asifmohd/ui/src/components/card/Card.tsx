eb350229b5ac10abae7756fbf5bc8d88cb8008c8
Asif Mohammed <asifm2882@gmail.com>
2020-03-08 23:00:20 +0530

    UI: get img ref and register scroll evnt

--- 01 - Spaces-iOS/asifmohd/ui/src/components/card/Card.tsx
+++ 01 - Spaces-iOS/asifmohd/ui/src/components/card/Card.tsx
@@ -1,18 +1,33 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import './Card.scss';
 
-const Card: React.FC<{ src: string }> = ({ src }) => (
-  <div className="Card-container">
-    <div className="Card">
-      <div className="Card-image">
-        <img src={src} alt="" />
-      </div>
-      <div className="Card-info">
-        <h2 className="Card-title">Relative UI Kit</h2>
-        <div className="Card-sub">7 PROJECTS</div>
+const Card: React.FC<{
+  space;
+  onScroll: Function;
+}> = ({ space, onScroll }) => {
+  const imgRef = useRef(null);
+  useEffect(() => {
+    onScroll(imgRef.current);
+  }, [onScroll]);
+  return (
+    <div className="Card-container">
+      <div className="Card">
+        <div className="Card-image">
+          <img src={space?.bg_image} alt="" ref={imgRef} />
+          <div className=" "></div>
+        </div>
+        <div className="Card-info">
+          <h2 className="Card-title">{space.title}</h2>
+          <div className="Card-sub">
+            {space.description.count +
+              ' ' +
+              space.description.type.toUpperCase() +
+              (space.description.count > 1 && 'S')}
+          </div>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Card;
