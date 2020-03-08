505f4c010fa5deab1e5fecec5be02c7ffc02cea3
Asif Mohammed <asifm2882@gmail.com>
2020-03-08 22:59:58 +0530

    UI: add horizontal parellax

--- 01 - Spaces-iOS/asifmohd/ui/src/containers/Main/Main.tsx
+++ 01 - Spaces-iOS/asifmohd/ui/src/containers/Main/Main.tsx
@@ -3,6 +3,7 @@ import Flickity, { FlickityOptions } from 'react-flickity-component';
 import './Main.scss';
 import '../../styles/flickity.css';
 import Card from '../../components/card/Card';
+import spaces from './../../model/spacesdata.json';
 
 const flickityOptions: FlickityOptions = {
   initialIndex: 0,
@@ -12,17 +13,39 @@ const flickityOptions: FlickityOptions = {
   prevNextButtons: false
 };
 
-const Main = () => (
-  <main>
-    <Flickity
-      className={'carousel'}
-      elementType={'div'}
-      options={flickityOptions}
-    >
-      <Card src={'http://unsplash.it/300/?image=129'} />
-      <Card src={'http://unsplash.it/300/?image=229'} />
-      <Card src={'http://unsplash.it/300/?image=529'} />
-    </Flickity>
-  </main>
-);
+export type ParellaxMap = Number[];
+const imgRefList: HTMLImageElement[] = [];
+let flktyRef: Flickity;
+
+const flkityParallaxOnScroll = imgRef => {
+  flktyRef.on('scroll', e => {
+    const slides = flktyRef.slides as Array<any>;
+    slides.forEach((slide, i) => {
+      let offset = (+(slide['target'] + flktyRef['x']) * -1) / 3;
+      imgRef[i].style.transform = 'translateX(' + offset + 'px) scale(1.3) ';
+    });
+  });
+};
+const scrollHandler = (imgRef: HTMLImageElement) => {
+  if (flktyRef && imgRef) {
+    imgRefList.push(imgRef);
+    if (imgRefList.length === spaces.length) flkityParallaxOnScroll(imgRefList);
+  }
+};
+const Main = () => {
+  return (
+    <main>
+      <Flickity
+        flickityRef={ref => (flktyRef = ref)}
+        className={'carousel'}
+        elementType={'div'}
+        options={flickityOptions}
+      >
+        {spaces.map((space, i) => (
+          <Card space={space} key={space.id} onScroll={scrollHandler} />
+        ))}
+      </Flickity>
+    </main>
+  );
+};
 export default Main;
