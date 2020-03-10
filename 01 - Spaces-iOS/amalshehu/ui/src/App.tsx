2cf11d8caf44344ad96e12a7618311905a7a76b2
amalshehu <amalshehu@gmail.com>
2020-03-10 17:09:25 +0530

    Basic grid setup

--- 01 - Spaces-iOS/amalshehu/ui/src/App.tsx
+++ 01 - Spaces-iOS/amalshehu/ui/src/App.tsx
@@ -1,16 +1,25 @@
 import React from 'react';
 import './App.scss';
 import Navbar from '../src/components/Navbar';
-
 function App() {
   return (
     <div className="App">
-      <h2>SPACES iOS</h2>
-      <header className="App-header">
-        <Navbar></Navbar>
-      </header>
-      <main>Hello world</main>
-      <footer>Footer is coming</footer>
+      {/* <span>
+        <i className="gg-bolt"></i>
+      </span> */}
+      <div className="wrapper">
+        <div className="header">
+          <header>
+            <Navbar></Navbar>
+          </header>
+        </div>
+        <div className="box content">
+          Content
+          <br /> More content than we had before so this column is now quite
+          tall.
+        </div>
+        <div className="box footer">Footer</div>
+      </div>
     </div>
   );
 }
