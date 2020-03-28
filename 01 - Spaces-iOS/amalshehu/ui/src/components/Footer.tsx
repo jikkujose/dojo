a8641fd8841e85ba13f402cf9887a72f542492e1
amalshehu <amalshehu@gmail.com>
2020-03-28 22:53:57 +0530

    [ 01 - Spaces-iOS] Add footer styles

---
+++ 01 - Spaces-iOS/amalshehu/ui/src/components/Footer.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Footer.scss';
+
+function Footer() {
+  return (
+    <nav className="App-header-nav">
+      <a href="#" className="App-footer-item">
+        <i className="gg-headset"></i>
+      </a>
+
+      <a href="#" className="App-footer-item">
+        <i className="gg-toolbox"></i>
+      </a>
+      <a href="#" className="App-footer-item">
+        <i className="gg-usb"></i>
+      </a>
+      <a href="#" className="App-footer-item">
+        <i className="gg-youtube"></i>
+      </a>
+    </nav>
+  );
+}
+
+export default Footer;
