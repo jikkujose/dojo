b3c22d10081be00851a98a80dff9ad9948b9b460
amalshehu <amalshehu@gmail.com>
2020-03-28 22:55:12 +0530

    [ 01 - Spaces-iOS] Add footer + Modify card styles

--- 01 - Spaces-iOS/amalshehu/ui/src/App.tsx
+++ 01 - Spaces-iOS/amalshehu/ui/src/App.tsx
@@ -2,28 +2,32 @@ import React from 'react';
 import './App.scss';
 import SwipeableViews from 'react-swipeable-views';
 import Navbar from '../src/components/Navbar';
+import Footer from './components/Footer';
 const styles = {
   slide: {
-    padding: 15,
-    minHeight: 100,
-    color: '#fff'
+    // padding: 15,
+    minHeight: 100
   },
-  slide1: {
-    background: '#FEA900'
-  },
-  slide2: {
-    background: '#B3DC4A'
-  },
-  slide3: {
-    background: '#6AC0FF'
-  }
+  slide1: {},
+  slide2: {},
+  slide3: {}
 };
+const card = (
+  <div className="card">
+    <div className="card-image"></div>
+    <div className="card-text">
+      <h2>Nike performance</h2>
+      <p>7 Products</p>
+    </div>
+  </div>
+);
 function App() {
   return (
     <div className="App">
-      {/* <span>
+      <span>
         <i className="gg-bolt"></i>
-      </span> */}
+      </span>
+
       <div className="wrapper">
         <div className="header">
           <header>
@@ -33,17 +37,19 @@ function App() {
         <div className="box content">
           <SwipeableViews>
             <div style={Object.assign({}, styles.slide, styles.slide1)}>
-              slide n°1
+              {card}
             </div>
             <div style={Object.assign({}, styles.slide, styles.slide2)}>
-              slide n°2
+              {card}
             </div>
             <div style={Object.assign({}, styles.slide, styles.slide3)}>
-              slide n°3
+              {card}
             </div>
           </SwipeableViews>
         </div>
-        <div className="box footer">Footer</div>
+        <div className="box footer">
+          <Footer></Footer>
+        </div>
       </div>
     </div>
   );
