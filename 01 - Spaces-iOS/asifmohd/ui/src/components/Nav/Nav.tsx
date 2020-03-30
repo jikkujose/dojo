bd93916d0681d96d0156fe2aac4d3dff1b7457bc
Asif Mohammed <asifm2882@gmail.com>
2020-03-30 15:45:19 +0530

    UI: pass selected as prop to nav

--- 01 - Spaces-iOS/asifmohd/ui/src/components/Nav/Nav.tsx
+++ 01 - Spaces-iOS/asifmohd/ui/src/components/Nav/Nav.tsx
@@ -3,13 +3,16 @@ import "./Nav.scss"
 import { navData } from "model/navdata"
 import Search from "icons/Search"
 
-const Nav = () => (
+const Nav = (selected: number = 0) => (
   <nav>
     <ul className="Nav-container">
       {navData.map((n, i) => (
-        <li key={i} className={`Nav-list ${i === 0 ? "Nav-active" : ""}`}>
+        <li
+          key={i}
+          className={`Nav-list ${i === selected ? "Nav-active" : ""}`}
+        >
           <a
-            className={`Nav-item ${i === 0 ? "Nav-active-text" : ""}`}
+            className={`Nav-item ${i === selected ? "Nav-active-text" : ""}`}
             href={n.href}
           >
             {n.title}
