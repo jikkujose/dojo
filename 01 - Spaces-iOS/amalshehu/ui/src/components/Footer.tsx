a93c55b911ce0f9371839ad46c0dc0a43c80db34
amalshehu <amalshehu@gmail.com>
2020-04-01 00:13:10 +0530

    [01 - Spaces-iOS] Fix valid url lint error

--- 01 - Spaces-iOS/amalshehu/ui/src/components/Footer.tsx
+++ 01 - Spaces-iOS/amalshehu/ui/src/components/Footer.tsx
@@ -3,18 +3,18 @@ import './Footer.scss';
 
 function Footer() {
   return (
-    <nav className="App-header-nav">
-      <a href="#" className="App-footer-item">
+    <nav className="App-footer-nav">
+      <a href="/" className="App-footer-item">
         <i className="gg-headset"></i>
       </a>
 
-      <a href="#" className="App-footer-item">
+      <a href="/" className="App-footer-item">
         <i className="gg-toolbox"></i>
       </a>
-      <a href="#" className="App-footer-item">
+      <a href="/" className="App-footer-item">
         <i className="gg-usb"></i>
       </a>
-      <a href="#" className="App-footer-item">
+      <a href="/" className="App-footer-item">
         <i className="gg-youtube"></i>
       </a>
     </nav>
