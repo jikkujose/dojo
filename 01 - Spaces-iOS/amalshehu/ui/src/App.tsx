867521ac2e179526cce6794be465005f9b963d6e
amalshehu <amalshehu@gmail.com>
2020-04-01 00:04:54 +0530

    [ 01 - Spaces-iOS] Fix Detail Modal + Add Detail component

--- 01 - Spaces-iOS/amalshehu/ui/src/App.tsx
+++ 01 - Spaces-iOS/amalshehu/ui/src/App.tsx
@@ -3,53 +3,57 @@ import './App.scss';
 import SwipeableViews from 'react-swipeable-views';
 import Navbar from '../src/components/Navbar';
 import Footer from './components/Footer';
+import { ProductDetail } from './components/ProductDetail';
+import { useModal, Modal } from 'react-morphing-modal';
+import 'react-morphing-modal/dist/ReactMorphingModal.css';
+import Card, {
+  CardPrimaryContent,
+  CardMedia,
+  CardActions,
+  CardActionButtons,
+  CardActionIcons
+} from '@material/react-card';
+
 const styles = {
-  slide: {
-    // padding: 15,
-    minHeight: 100
-  },
-  slide1: {},
-  slide2: {},
-  slide3: {}
+  root: {
+    padding: '0 20px'
+  }
 };
-const card = (
-  <div className="card">
-    <div className="card-image"></div>
-    <div className="card-text">
-      <h2>Nike performance</h2>
-      <p>7 Products</p>
-    </div>
-  </div>
-);
+
 function App() {
+  const { close, modalProps, getTriggerProps } = useModal();
   return (
     <div className="App">
-      <span>
-        <i className="gg-bolt"></i>
-      </span>
+      <div className="Layout">
+        <header className="Header">
+          <Navbar></Navbar>
+        </header>
+        <section className="Content">
+          <ul className="Products">
+            <SwipeableViews style={styles.root}>
+              <li>
+                <span>
+                  <button
+                    {...getTriggerProps({ id: 'productDetail1' })}
+                    className="Btn Product_btn"
+                  >
+                    Relate UI Kit
+                  </button>
+                </span>
+              </li>
 
-      <div className="wrapper">
-        <div className="header">
-          <header>
-            <Navbar></Navbar>
-          </header>
-        </div>
-        <div className="box content">
-          <SwipeableViews>
-            <div style={Object.assign({}, styles.slide, styles.slide1)}>
-              {card}
-            </div>
-            <div style={Object.assign({}, styles.slide, styles.slide2)}>
-              {card}
-            </div>
-            <div style={Object.assign({}, styles.slide, styles.slide3)}>
-              {card}
-            </div>
-          </SwipeableViews>
-        </div>
-        <div className="box footer">
+              <li>
+                <span>InVision Craft</span>
+              </li>
+            </SwipeableViews>
+            <Modal {...modalProps}>
+              <ProductDetail closeModal={close} />
+            </Modal>
+          </ul>
+        </section>
+        <footer className="Footer">
           <Footer></Footer>
-        </div>
+        </footer>
       </div>
     </div>
   );
