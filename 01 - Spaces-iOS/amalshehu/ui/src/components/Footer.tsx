60baec4c1fb06984bc94a57c03d25308ddce2d8a
amalshehu <amalshehu@gmail.com>
2020-04-01 13:54:03 +0530

    [01 - Spaces-iOS] Fix footer icons

--- 01 - Spaces-iOS/amalshehu/ui/src/components/Footer.tsx
+++ 01 - Spaces-iOS/amalshehu/ui/src/components/Footer.tsx
@@ -1,23 +1,51 @@
 import React from 'react';
 import './Footer.scss';
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+import { makeStyles } from '@material-ui/core/styles';
+import FolderOpenIcon from '@material-ui/icons/FolderOpen';
+import PhotoCameraIcon from '@material-ui/icons/PhotoCamera';
+import AllInclusiveIcon from '@material-ui/icons/AllInclusive';
+import PersonOutlineIcon from '@material-ui/icons/PersonOutline';
+import { BottomNavigation } from '@material-ui/core';
+const useStyles = makeStyles({
+  root: {
+    height: 100
+  },
+  iconStyle: {
+    color: '#ff4090'
+  }
+});
 
 function Footer() {
+  const classes = useStyles();
+  const [value] = React.useState(0);
   return (
-    <nav className="App-footer-nav">
-      <a href="/" className="App-footer-item">
-        <i className="gg-headset"></i>
-      </a>
-
-      <a href="/" className="App-footer-item">
-        <i className="gg-toolbox"></i>
-      </a>
-      <a href="/" className="App-footer-item">
-        <i className="gg-usb"></i>
-      </a>
-      <a href="/" className="App-footer-item">
-        <i className="gg-youtube"></i>
-      </a>
-    </nav>
+    <BottomNavigation value={value} className={classes.root}>
+      <BottomNavigationAction
+        label="PhotoCamera"
+        value="PhotoCamera"
+        className="inActive"
+        icon={<PhotoCameraIcon />}
+      />
+      <BottomNavigationAction
+        label="FolderOpen"
+        value="FolderOpen"
+        className={classes.iconStyle}
+        icon={<FolderOpenIcon />}
+      />
+      <BottomNavigationAction
+        label="AllInclusive"
+        value="AllInclusive"
+        className="inActive"
+        icon={<AllInclusiveIcon />}
+      />
+      <BottomNavigationAction
+        label="PersonOutline"
+        value="PersonOutline"
+        className="inActive"
+        icon={<PersonOutlineIcon />}
+      />
+    </BottomNavigation>
   );
 }
 
