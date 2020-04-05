62d31547eea7da16404ad2411ca1c4bd864bc66d
amalshehu <amalshehu@gmail.com>
2020-04-05 14:20:57 +0530

    [01 - Spaces-iOS] Add selectors

--- 01 - Spaces-iOS/amalshehu/ui/src/app/App.tsx
+++ 01 - Spaces-iOS/amalshehu/ui/src/app/App.tsx
@@ -1,27 +1,41 @@
-import React from 'react';
-import './App.scss';
-import SwipeableViews from 'react-swipeable-views';
-import Navbar from '../components/Navbar';
-import Footer from '../components/Footer';
-import ProductCard from '../components/ProductCard';
-import { ProductDetail } from '../components/ProductDetail';
-import { useModal, Modal } from 'react-morphing-modal';
-import 'react-morphing-modal/dist/ReactMorphingModal.css';
-import FullScreenDialog from '../components/Dialog';
+import React from 'react'
+import './App.scss'
+import SwipeableViews from 'react-swipeable-views'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+import ProductCard from '../components/ProductCard'
+import { ProductDetail } from '../components/ProductDetail'
+import { useModal, Modal } from 'react-morphing-modal'
+import 'react-morphing-modal/dist/ReactMorphingModal.css'
+import FullScreenDialog from '../components/Dialog'
+import { setCurrentDisplayType } from 'features/productsDisplay/productsDisplaySlice'
+import { useSelector, useDispatch } from 'react-redux'
+import { RootState } from './rootReducer'
 
 const styles = {
   root: {
-    padding: '0 20px'
+    padding: '0 20px',
   },
 
   slide: {
     padding: 10,
-    minHeight: 100
-  }
-};
+    minHeight: 100,
+  },
+}
 
 function App() {
-  const { close, modalProps, getTriggerProps } = useModal();
+  const dispatch = useDispatch()
+
+  const showProductsList = () => {
+    dispatch(setCurrentDisplayType({ displayType: 'details' }))
+  }
+  const { close, modalProps, getTriggerProps } = useModal()
+  showProductsList()
+  const { displayType } = useSelector(
+    (state: RootState) => state.productsDisplay
+  )
+  console.log('displayType', displayType)
+
   return (
     <div className="App">
       <div className="Layout">
@@ -68,7 +82,7 @@ function App() {
         </footer>
       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
