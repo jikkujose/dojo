65b0f11e8fb9cc7654ec3ae552a9fee84c7f257a
amalshehu <amalshehu@gmail.com>
2020-04-05 13:55:41 +0530

    [01 - Spaces-iOS] FIx store path

--- 01 - Spaces-iOS/amalshehu/ui/src/index.tsx
+++ 01 - Spaces-iOS/amalshehu/ui/src/index.tsx
@@ -4,7 +4,7 @@ import './index.scss'
 import App from './app/App'
 import * as serviceWorker from './serviceWorker'
 import { Provider } from 'react-redux'
-import store from './redux/store'
+import store from './app/store'
 
 import '@material/react-card/dist/card.css'
 
