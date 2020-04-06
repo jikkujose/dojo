0f2da493f9884701d09ed9f89928c8eb2937305d
amalshehu <amalshehu@gmail.com>
2020-04-06 21:24:14 +0530

    [01 - Spaces-iOS] Dispatch setCurrentDisplayType action + Select displayType

--- 01 - Spaces-iOS/amalshehu/ui/src/app/App.tsx
+++ 01 - Spaces-iOS/amalshehu/ui/src/app/App.tsx
@@ -1,35 +1,18 @@
 import React from 'react'
 import './App.scss'
-import SwipeableViews from 'react-swipeable-views'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
-import ProductCard from '../components/ProductCard'
-import { ProductDetail } from '../components/ProductDetail'
-import { useModal, Modal } from 'react-morphing-modal'
-import 'react-morphing-modal/dist/ReactMorphingModal.css'
-import FullScreenDialog from '../components/Dialog'
 import { setCurrentDisplayType } from 'features/productsDisplay/productsDisplaySlice'
 import { useSelector, useDispatch } from 'react-redux'
 import { RootState } from './rootReducer'
-
-const styles = {
-  root: {
-    padding: '0 20px',
-  },
-
-  slide: {
-    padding: 10,
-    minHeight: 100,
-  },
-}
+import { ProductsList } from 'features/ProductList/ProductList'
 
 function App() {
   const dispatch = useDispatch()
 
   const showProductsList = () => {
-    dispatch(setCurrentDisplayType({ displayType: 'details' }))
+    dispatch(setCurrentDisplayType({ displayType: 'list' }))
   }
-  const { close, modalProps, getTriggerProps } = useModal()
   showProductsList()
   const { displayType } = useSelector(
     (state: RootState) => state.productsDisplay
@@ -43,39 +26,7 @@ function App() {
           <Navbar></Navbar>
         </header>
         <section className="Content">
-          <ul className="Products">
-            <SwipeableViews style={styles.root}>
-              <div style={Object.assign({}, styles.slide)}>
-                <FullScreenDialog></FullScreenDialog>
-              </div>
-              <div style={Object.assign({}, styles.slide)}>
-                <ProductCard></ProductCard>
-              </div>
-              <div style={Object.assign({}, styles.slide)}>
-                <ProductCard></ProductCard>
-              </div>
-              <div style={Object.assign({}, styles.slide)}>
-                <ProductCard></ProductCard>
-              </div>
-              <li>
-                <span>
-                  <button
-                    {...getTriggerProps({ id: 'productDetail1' })}
-                    className="Btn Product_btn"
-                  >
-                    Relate UI Kit
-                  </button>
-                </span>
-              </li>
-
-              <li>
-                <span>InVision Craft</span>
-              </li>
-            </SwipeableViews>
-            <Modal {...modalProps}>
-              <ProductDetail closeModal={close} />
-            </Modal>
-          </ul>
+          {/* <ProductsList products={}></ProductsList> */}
         </section>
         <footer className="Footer">
           <Footer></Footer>
