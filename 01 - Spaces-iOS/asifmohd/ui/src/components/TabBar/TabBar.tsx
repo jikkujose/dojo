146f0cd5fc0c247ba8c3351adcf87155de8a3db2
Asif Mohammed <asifm2882@gmail.com>
2020-04-08 05:11:52 +0530

    UI: hide tabBar on click

--- 01 - Spaces-iOS/asifmohd/ui/src/components/TabBar/TabBar.tsx
+++ 01 - Spaces-iOS/asifmohd/ui/src/components/TabBar/TabBar.tsx
@@ -1,19 +1,40 @@
-import React from "react"
+import React, { useState } from "react"
 import "./TabBar.scss"
 import Diamonds from "icons/Diamonds"
 import Folder from "icons/Folder"
 import Inbox from "icons/Inbox"
 import User from "icons/User"
+import { motion } from "framer-motion"
 
-const TabBar = () => (
-  <footer>
-    <div className="Footer-contianer">
-      <Diamonds />
-      <Folder />
-      <Inbox />
-      <User />
-    </div>
-  </footer>
-)
+const variants = {
+  hidden: {
+    y: 100,
+    opacity: 0,
+    transition: {
+      delay: 3,
+    },
+    transitionEnd: {
+      display: "none",
+    },
+  },
+  visible: { y: 0 },
+}
+const TabBar = () => {
+  const [checker, setChecker] = useState(false)
+  return (
+    <motion.footer
+      initial="false"
+      animate={checker ? "hidden" : "visible"}
+      variants={variants}
+    >
+      <div className="Footer-contianer" onClick={() => setChecker(true)}>
+        <Diamonds />
+        <Folder />
+        <Inbox />
+        <User />
+      </div>
+    </motion.footer>
+  )
+}
 
 export default TabBar
