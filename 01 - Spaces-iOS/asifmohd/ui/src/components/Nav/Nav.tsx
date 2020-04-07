27bed9d874124277b56eca95a8cb5e62bd9f0600
Asif Mohammed <asifm2882@gmail.com>
2020-04-08 05:11:37 +0530

    UI: hide nav on click

--- 01 - Spaces-iOS/asifmohd/ui/src/components/Nav/Nav.tsx
+++ 01 - Spaces-iOS/asifmohd/ui/src/components/Nav/Nav.tsx
@@ -1,29 +1,49 @@
-import React from "react"
+import React, { useState } from "react"
 import "./Nav.scss"
 import { navData } from "model/navdata"
 import Search from "icons/Search"
+import { motion } from "framer-motion"
 
-const Nav = ({ selected = 0 }) => (
-  <nav>
-    <ul className="Nav-container">
-      {navData.map((n, i) => (
-        <li
-          key={i}
-          className={`Nav-list ${i === selected ? "Nav-active" : ""}`}
-        >
-          <a
-            className={`Nav-item ${i === selected ? "Nav-active-text" : ""}`}
-            href={n.href}
+const variants = {
+  hidden: {
+    y: -100,
+    transition: {
+      delay: 1,
+    },
+  },
+  visible: { y: 0 },
+}
+
+const Nav = ({ selected = 0 }) => {
+  const [checker, setChecker] = useState(false)
+  console.log(checker)
+  return (
+    <motion.nav
+      initial="false"
+      animate={checker ? "hidden" : "visible"}
+      variants={variants}
+    >
+      <ul className="Nav-container">
+        {navData.map((n, i) => (
+          <li
+            key={i}
+            className={`Nav-list ${i === selected ? "Nav-active" : ""}`}
+            onClick={() => setChecker(true)}
           >
-            {n.title}
-          </a>
+            <a
+              className={`Nav-item ${i === selected ? "Nav-active-text" : ""}`}
+              href={n.href}
+            >
+              {n.title}
+            </a>
+          </li>
+        ))}
+        <li className="Nav-list">
+          <Search />
         </li>
-      ))}
-      <li className="Nav-list">
-        <Search />
-      </li>
-    </ul>
-  </nav>
-)
+      </ul>
+    </motion.nav>
+  )
+}
 
 export default Nav
