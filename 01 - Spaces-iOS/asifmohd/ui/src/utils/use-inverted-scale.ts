710445a2b24da0366a66654162709be0d1700d7e
Asif Mohammed <asifm2882@gmail.com>
2020-04-09 18:47:30 +0530

    UI: inverted border radius fn

---
+++ 01 - Spaces-iOS/asifmohd/ui/src/utils/use-inverted-scale.ts
@@ -0,0 +1,37 @@
+import { useMotionValue, useInvertedScale } from "framer-motion"
+import { useEffect } from "react"
+
+export const useInvertedBorderRadius = (radius: number) => {
+  const scaleX = useMotionValue(1)
+  const scaleY = useMotionValue(1)
+  const inverted = useInvertedScale({ scaleX, scaleY })
+  const borderRadius = useMotionValue(`${radius}px`)
+
+  useEffect(() => {
+    function updateRadius() {
+      const latestX = inverted.scaleX.get()
+      const latestY = inverted.scaleY.get()
+      const xRadius = latestX * radius + "px"
+      const yRadius = latestY * radius + "px"
+
+      borderRadius.set(`${xRadius} ${yRadius}`)
+    }
+
+    const unsubScaleX = inverted.scaleX.onChange(updateRadius)
+    const unsubScaleY = inverted.scaleY.onChange(updateRadius)
+
+    return () => {
+      unsubScaleX()
+      unsubScaleY()
+    }
+  }, [radius])
+
+  return {
+    scaleX,
+    scaleY,
+    borderTopLeftRadius: borderRadius,
+    borderTopRightRadius: borderRadius,
+    borderBottomLeftRadius: borderRadius,
+    borderBottomRightRadius: borderRadius,
+  }
+}
