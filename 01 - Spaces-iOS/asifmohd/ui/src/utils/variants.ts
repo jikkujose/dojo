59e18e726c92c01175a97bf132080591b0b9c43e
Asif Mohammed <asifm2882@gmail.com>
2020-04-09 19:08:50 +0530

    UI: create variants

---
+++ 01 - Spaces-iOS/asifmohd/ui/src/utils/variants.ts
@@ -0,0 +1,17 @@
+export const tabBarVariants = {
+  hidden: {
+    y: 100,
+    opacity: 0,
+  },
+  visible: {
+    y: 0,
+    opacity: 1,
+  },
+}
+
+export const navBarVariants = {
+  hidden: {
+    y: -100,
+  },
+  visible: { y: 0 },
+}
