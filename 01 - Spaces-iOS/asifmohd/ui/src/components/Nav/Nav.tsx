651b6a92681b30bc93dc11846475b4b4675e74e9
Asif Mohammed <asifm2882@gmail.com>
2020-04-09 19:09:31 +0530

    UI: sync nav animation with card

--- 01 - Spaces-iOS/asifmohd/ui/src/components/Nav/Nav.tsx
+++ 01 - Spaces-iOS/asifmohd/ui/src/components/Nav/Nav.tsx
@@ -1,34 +1,27 @@
-import React, { useState } from "react"
+import React, { useContext } from "react"
 import "./Nav.scss"
 import { navData } from "model/navdata"
 import Search from "icons/Search"
 import { motion } from "framer-motion"
-
-const variants = {
-  hidden: {
-    y: -100,
-    transition: {
-      delay: 1,
-    },
-  },
-  visible: { y: 0 },
-}
+import { store } from "store/store"
+import { openSpring, closeSpring } from "utils/animation"
+import { navBarVariants } from "utils/variants"
 
 const Nav = ({ selected = 0 }) => {
-  const [checker, setChecker] = useState(false)
-  console.log(checker)
+  const { state } = useContext(store)
+
   return (
     <motion.nav
       initial="false"
-      animate={checker ? "hidden" : "visible"}
-      variants={variants}
+      animate={state.slide.isSelected ? "hidden" : "visible"}
+      transition={state.slide.isSelected ? openSpring : closeSpring}
+      variants={navBarVariants}
     >
       <ul className="Nav-container">
         {navData.map((n, i) => (
           <li
             key={i}
             className={`Nav-list ${i === selected ? "Nav-active" : ""}`}
-            onClick={() => setChecker(true)}
           >
             <a
               className={`Nav-item ${i === selected ? "Nav-active-text" : ""}`}
