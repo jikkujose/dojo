5e3397c9d223c631ab6cef9ef596b480aa74c46b
Asif Mohammed <asifm2882@gmail.com>
2020-04-09 19:05:05 +0530

    UI: create global store

---
+++ 01 - Spaces-iOS/asifmohd/ui/src/store/store.tsx
@@ -0,0 +1,39 @@
+import { InitContextProps } from "./reducer"
+import React, { createContext, useReducer } from "react"
+import { reducer, State } from "store/reducer"
+import { motion } from "framer-motion"
+import { openSpring, closeSpring } from "utils/animation"
+
+const initialState: State = {
+  route: {
+    path: "",
+    prev: "",
+  },
+  slide: {
+    isSelected: false,
+    selectedIndex: 0,
+  },
+}
+const store = createContext({} as InitContextProps)
+const { Provider } = store
+
+const StateProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState)
+  return (
+    <Provider value={{ state, dispatch }}>
+      <motion.section
+        className="App-container"
+        style={{
+          gridTemplateRows: state?.slide.isSelected
+            ? `0 minmax(400px, 1fr) 0`
+            : `95px minmax(400px, 1fr) 95px`,
+        }}
+        transition={state.slide.isSelected ? openSpring : closeSpring}
+      >
+        {children}
+      </motion.section>
+    </Provider>
+  )
+}
+
+export { store, StateProvider }
