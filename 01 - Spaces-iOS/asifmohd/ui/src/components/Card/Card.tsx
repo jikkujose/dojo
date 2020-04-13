5ed2b96abc66f41a67732d963a5c0ddb6a1331e3
Asif Mohammed <asifm2882@gmail.com>
2020-04-13 18:11:09 +0530

    UI: create inner slider

--- 01 - Spaces-iOS/asifmohd/ui/src/components/Card/Card.tsx
+++ 01 - Spaces-iOS/asifmohd/ui/src/components/Card/Card.tsx
@@ -7,10 +7,13 @@ import {
 } from "framer-motion"
 import { Link } from "react-router-dom"
 import "./Card.scss"
-import { openSpring, closeSpring } from "utils/animation"
+import { openSpring, closeSpring, closeInertial } from "utils/animation"
 import { store } from "store/store"
 import { useInvertedBorderRadius } from "utils/use-inverted-scale"
 import { CloseButton } from "icons/CloseButton"
+import Info from "./CardInfo/Info"
+import Detail from "./CardDetail/Detail"
+import { infoVariants, ImageVariants } from "utils/variants"
 
 const Card: React.FC<{
   space
@@ -37,6 +40,17 @@ const Card: React.FC<{
     const zIndex = useMotionValue(isSelected ? 2 : 0)
     const inverted = useInvertedBorderRadius(isSelected ? 0 : 10)
     const invertedImg = useInvertedScale()
+    const openInfoStyle = {
+      title: {
+        fontSize: 24,
+        letterSpacing: 0.8,
+        color: "#c0c0c0",
+      },
+      sub: {
+        fontSize: 13,
+        opacity: 0.8,
+      },
+    }
 
     useEffect(() => {
       onScroll(imgRef.current)
@@ -54,10 +68,9 @@ const Card: React.FC<{
       if (isSelected) {
         zIndex.set(2)
         navBarToggle(false)
-        // if (selectedIndex && scaleX >= 0.7 && scaleX <= 0.8) {
-        // }
+        // if()
       } else {
-        if (!isSelected && latest.scaleX < 1.01) {
+        if (!isSelected && scaleX < 1.01) {
           zIndex.set(0)
         }
         if (scaleX < 1.2 && scaleX > 1) {
@@ -67,73 +80,87 @@ const Card: React.FC<{
     }
 
     return (
-      <AnimatePresence>
-        <div className="Card-container">
-          <div
-            className={`Card ${isSelected && "open"}`}
-            style={{
-              transform: isSelected ? `translateX(${-1 * translateX}%)` : "",
-            }}
+      <motion.div className="Card-container">
+        <motion.div
+          className={`Card ${isSelected && "open"}`}
+          style={{
+            transform: isSelected ? `translateX(${-1 * translateX}%)` : "",
+          }}
+        >
+          <motion.div
+            className="Card-content"
+            style={{ ...inverted, zIndex }}
+            layoutTransition={isSelected ? openSpring : closeSpring}
+            onUpdate={checkZIndex}
+            // onAnimationComplete={}
           >
             <motion.div
-              className="Card-content"
-              style={{ ...inverted, zIndex }}
-              layoutTransition={isSelected ? openSpring : closeSpring}
-              onUpdate={checkZIndex}
+              className="Card-image"
+              // initial={false}
+              variants={ImageVariants}
+              style={{ ...invertedImg, originX: 0, originY: 0 }}
+              animate={!isSelected ? "visible" : "hidden"}
+              transition={isSelected ? openSpring : closeSpring}
             >
-              <motion.div
-                className="Card-image"
+              <motion.img
+                data-flickity-lazyload={space?.bg_image}
+                alt=""
+                ref={imgRef}
                 initial={false}
-                style={{ ...invertedImg, originX: 0, originY: 0 }}
-                animate={!isSelected ? { height: 304 } : { height: 400 }}
-                transition={isSelected ? openSpring : closeSpring}
-              >
-                <motion.img
-                  data-flickity-lazyload={space?.bg_image}
-                  alt=""
-                  ref={imgRef}
-                  initial={false}
-                  transition={closeSpring}
-                />
-                {/* <div className=""></div> */}
-              </motion.div>
-              {isSelected && <CloseButton close={close} />}
-              <div className="Card-info-wrapper">
-                {!isSelected && (
-                  <motion.div
-                    className="Card-info"
-                    initial={{ opacity: 0 }}
-                    animate={{ opacity: 1, transition: { delay: 0.6 } }}
-                    exit={{ opacity: 0 }}
-                  >
-                    <h2 className="Card-title">{space.title}</h2>
-                    <div className="Card-sub">
-                      {space.description.count +
-                        " " +
-                        space.description.type +
-                        (space.description.count > 1 && "s")}
-                    </div>
-                    {space.face_thumbs.map(
-                      (thumb, i) =>
-                        i < 3 && (
-                          <span
-                            className={"Card-thumb Card-thumb__" + i}
-                            key={i}
-                          >
-                            <img src={thumb.image} alt={thumb.name} />
-                          </span>
-                        )
-                    )}
-                  </motion.div>
-                )}
-              </div>
+                transition={closeSpring}
+              />
+              {/* <div className=""></div> */}
             </motion.div>
-          </div>
-          {!isSelected && selectedIndex && (
-            <Link to={space.id} className={`Card-open-link`} />
-          )}
-        </div>
-      </AnimatePresence>
+            {isSelected && (
+              <>
+                <CloseButton close={close} />
+                <Detail />
+                <div className="Card-desc-title">
+                  <AnimatePresence exitBeforeEnter>
+                    <Info
+                      title={space.title}
+                      description={space.description}
+                      style={openInfoStyle}
+                      variants={infoVariants}
+                      initial={"hidden"}
+                      animate={"visible"}
+                      exit={"hidden"}
+                      transition={closeInertial}
+                    />
+                  </AnimatePresence>
+                </div>
+              </>
+            )}
+            {!isSelected && (
+              <div className="Card-info-wrapper">
+                <motion.div
+                  className="Card-info"
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1, transition: { delay: 0.2 } }}
+                  exit={{ opacity: 0 }}
+                >
+                  <Info
+                    title={space.title}
+                    description={space.description}
+                    style={{}}
+                  />
+                  {space.face_thumbs.map(
+                    (thumb, i) =>
+                      i < 3 && (
+                        <span className={"Card-thumb Card-thumb__" + i} key={i}>
+                          <img src={thumb.image} alt={thumb.name} />
+                        </span>
+                      )
+                  )}
+                </motion.div>
+              </div>
+            )}
+          </motion.div>
+        </motion.div>
+        {!isSelected && selectedIndex && (
+          <Link to={space.id} className={`Card-open-link`} />
+        )}
+      </motion.div>
     )
   },
   (prev, next) =>
