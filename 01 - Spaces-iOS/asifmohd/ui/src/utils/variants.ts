79cad80106b7358385e8fb673b437f267b691fda
Asif Mohammed <asifm2882@gmail.com>
2020-04-13 16:06:32 +0530

    UI: add new animation varients

--- 01 - Spaces-iOS/asifmohd/ui/src/utils/variants.ts
+++ 01 - Spaces-iOS/asifmohd/ui/src/utils/variants.ts
@@ -15,3 +15,34 @@ export const navBarVariants = {
   },
   visible: { y: 0 },
 }
+
+export const closeBtnVariants = {
+  hidden: {
+    opacity: 0,
+    y: -100,
+  },
+  visible: { y: 0, opacity: 1, transition: { delay: 0.4, duration: 0.3 } },
+}
+
+export const infoVariants = {
+  hidden: {
+    opacity: 0,
+    y: 25,
+  },
+  visible: { y: 0, opacity: 1, transition: { duration: 0.4, delay: 0.5 } },
+}
+
+export const flicktyVariants = {
+  hidden: {
+    opacity: 0,
+    x: "100%",
+  },
+  visible: { opacity: 1, x: 0, transition: { delay: 0.5, duration: 0.4 } },
+}
+
+export const ImageVariants = {
+  hidden: {
+    height: 400,
+  },
+  visible: { height: 304 },
+}
