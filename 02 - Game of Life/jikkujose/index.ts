ecd99ac2559a712bc7ad20e0724dc2b7d40860a7
Jikku Jose <jikkujose@gmail.com>
2020-04-17 19:02:05 +0530

    02 | Add p5 starting code

--- 02 - Game of Life/jikkujose/index.ts
+++ 02 - Game of Life/jikkujose/index.ts
@@ -1,1 +1,15 @@
-console.log("hw")
+import p5 from "p5"
+
+const sketch = s => {
+  s.setup = () => {
+    s.createCanvas(512, 512)
+    s.background(230)
+    s.noLoop()
+  }
+
+  s.draw = () => {
+    console.log("draw")
+  }
+}
+
+new p5(sketch)
