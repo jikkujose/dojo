82c398e3f66da260b729e316d7706706cd54d0fd
Asif Mohammed <asifm2882@gmail.com>
2020-04-17 23:05:50 +0530

    02-Life: generate inital grid

--- 02 - Game of Life/asifmohd/game-of-life/src/App.tsx
+++ 02 - Game of Life/asifmohd/game-of-life/src/App.tsx
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useCallback } from "react"
+import "./App.css"
 
-function App() {
+let ROW = 30
+let COLUMN = 30
+
+const gridGenerator = (row: number, col: number) =>
+  Array.from(Array(ROW)).map(() => Array.from(Array(COLUMN)).fill(0))
+
+const App = () => {
+  const [grid, setGrid] = useState(gridGenerator.call(null, ROW, COLUMN))
+
+  const setGridState = (i: number, j: number) => {
+    const newGrid = JSON.parse(JSON.stringify(grid))
+    newGrid[i][j] = grid[i][j] ? 0 : 1
+    setGrid(newGrid)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main className="Main-container">
+      <div
+        className="Grid-container"
+        style={{
+          gridTemplateColumns: `repeat(${COLUMN}, 25px)`,
+        }}
+      >
+        {grid.map((row, i) =>
+          row.map((col, j) => (
+            <div
+              key={`${i}${j}`}
+              onClick={() => setGridState(i, j)}
+              style={{
+                width: 25,
+                height: 25,
+                backgroundColor: grid[i][j] ? "#212121" : "#f1f1f1",
+                border: "solid 1px black",
+              }}
+            ></div>
+          ))
+        )}
+      </div>
+    </main>
+  )
 }
 
-export default App;
+export default App
