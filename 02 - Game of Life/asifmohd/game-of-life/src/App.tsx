b8775bcb20d217ee23d158b884ed0f69367c6d77
Asif Mohammed <asifm2882@gmail.com>
2020-04-18 04:27:29 +0530

    02-LIfe: setGrideState fn

--- 02 - Game of Life/asifmohd/game-of-life/src/App.tsx
+++ 02 - Game of Life/asifmohd/game-of-life/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from "react"
+import React, { useState, useCallback, useRef, useEffect } from "react"
 import "./App.css"
 
 let ROW = 30
@@ -24,35 +24,35 @@ const App = () => {
   const [isRunning, setisRunning] = useState(false)
 
   const setGridState = (i: number, j: number) => {
-    const newGrid = JSON.parse(JSON.stringify(grid))
+    const newGrid = deepClone(grid)
     newGrid[i][j] = grid[i][j] ? 0 : 1
     setGrid(newGrid)
   }
 
   return (
-    <main className="Main-container">
-      <div
-        className="Grid-container"
-        style={{
-          gridTemplateColumns: `repeat(${COLUMN}, 25px)`,
-        }}
-      >
-        {grid.map((row, i) =>
-          row.map((col, j) => (
-            <div
-              key={`${i}${j}`}
-              onClick={() => setGridState(i, j)}
-              style={{
-                width: 25,
-                height: 25,
-                backgroundColor: grid[i][j] ? "#212121" : "#f1f1f1",
-                border: "solid 1px black",
-              }}
-            ></div>
-          ))
-        )}
-      </div>
-    </main>
+      <main className="Main-container">
+        <div
+          className="Grid-container"
+          style={{
+            gridTemplateColumns: `repeat(${COLUMN}, 25px)`,
+          }}
+        >
+          {grid.map((row, i) =>
+            row.map((col, j) => (
+              <div
+                key={`${i}${j}`}
+                onClick={() => setGridState(i, j)}
+                style={{
+                  width: 25,
+                  height: 25,
+                  backgroundColor: grid[i][j] ? "#212121" : "#f1f1f1",
+                  border: "solid 1px black",
+                }}
+              ></div>
+            ))
+          )}
+        </div>
+      </main>
   )
 }
 
