41f34f5315f21b08f050d6d14adc08aa8a69f117
jibinmathew69 <jibinmathew.69@gmail.com>
2020-04-18 08:38:50 +0530

    Fix flask boilerplate

--- web_app.py
+++ web_app.py
@@ -2,11 +2,12 @@ from flask import Flask, redirect, Response, request
 import time
 from game import Game
 
-@app.route('/', methods=['GET']):
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
 def index():
     pass
 
 if __name__ == '__main__':
     game = None
-    app = Flask(__name__)
     app.run()
