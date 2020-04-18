255243089cadf6ddfcd47e88a806cbf8c1244953
jibinmathew69 <jibinmathew.69@gmail.com>
2020-04-18 08:38:12 +0530

    Add index route

--- web_app.py
+++ web_app.py
@@ -2,6 +2,10 @@ from flask import Flask, redirect, Response, request
 import time
 from game import Game
 
+@app.route('/', methods=['GET']):
+def index():
+    pass
+
 if __name__ == '__main__':
     game = None
     app = Flask(__name__)
