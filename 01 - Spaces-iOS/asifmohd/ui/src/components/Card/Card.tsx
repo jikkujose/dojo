3782867bd27841963d1e5953c212e767d8fa2d63
Asif Mohammed <asifm2882@gmail.com>
2020-04-22 21:55:06 +0530

    UI: refactor Info component

--- 01 - Spaces-iOS/asifmohd/ui/src/components/Card/Card.tsx
+++ 01 - Spaces-iOS/asifmohd/ui/src/components/Card/Card.tsx
@@ -1,16 +1,11 @@
 import React, { useEffect, useRef, useContext, memo } from "react"
-import {
-  motion,
-  useInvertedScale,
-  AnimatePresence,
-  useMotionValue,
-} from "framer-motion"
+import { motion, useInvertedScale, useMotionValue } from "framer-motion"
 import { Link } from "react-router-dom"
 import "./Card.scss"
 import { openSpring, closeSpring, closeInertial } from "utils/animation"
 import { store } from "store/store"
 import { useInvertedBorderRadius } from "utils/use-inverted-scale"
-import { CloseButton } from "icons/CloseButton"
+import CloseBtn from "ui/CloseBtn/CloseBtn"
 import Info from "./CardInfo/Info"
 import Detail from "./CardDetail/Detail"
 import { infoVariants, ImageVariants } from "utils/variants"
@@ -92,11 +87,9 @@ const Card: React.FC<{
             style={{ ...inverted, zIndex }}
             layoutTransition={isSelected ? openSpring : closeSpring}
             onUpdate={checkZIndex}
-            // onAnimationComplete={}
           >
             <motion.div
               className="Card-image"
-              // initial={false}
               variants={ImageVariants}
               style={{ ...invertedImg, originX: 0, originY: 0 }}
               animate={!isSelected ? "visible" : "hidden"}
@@ -109,51 +102,33 @@ const Card: React.FC<{
                 initial={false}
                 transition={closeSpring}
               />
-              {/* <div className=""></div> */}
             </motion.div>
             {isSelected && (
               <>
-                <CloseButton close={close} />
+                <CloseBtn close={close} />
+                <Info
+                  title={space.title}
+                  description={space.description}
+                  style={openInfoStyle}
+                  isDescTitle={true}
+                  animation={{
+                    variants: infoVariants,
+                    initial: "hidden",
+                    animate: "visible",
+                    exit: "hidden",
+                    transition: closeInertial,
+                  }}
+                />
                 <Detail />
-                <div className="Card-desc-title">
-                  <AnimatePresence exitBeforeEnter>
-                    <Info
-                      title={space.title}
-                      description={space.description}
-                      style={openInfoStyle}
-                      variants={infoVariants}
-                      initial={"hidden"}
-                      animate={"visible"}
-                      exit={"hidden"}
-                      transition={closeInertial}
-                    />
-                  </AnimatePresence>
-                </div>
               </>
             )}
             {!isSelected && (
-              <div className="Card-info-wrapper">
-                <motion.div
-                  className="Card-info"
-                  initial={{ opacity: 0 }}
-                  animate={{ opacity: 1, transition: { delay: 0.2 } }}
-                  exit={{ opacity: 0 }}
-                >
-                  <Info
-                    title={space.title}
-                    description={space.description}
-                    style={{}}
-                  />
-                  {space.face_thumbs.map(
-                    (thumb, i) =>
-                      i < 3 && (
-                        <span className={"Card-thumb Card-thumb__" + i} key={i}>
-                          <img src={thumb.image} alt={thumb.name} />
-                        </span>
-                      )
-                  )}
-                </motion.div>
-              </div>
+              <Info
+                title={space.title}
+                description={space.description}
+                thumbs={space?.face_thumbs}
+                style={{}}
+              />
             )}
           </motion.div>
         </motion.div>
--- 01 - Spaces-iOS/asifmohd/ui/src/components/Card/CardInfo/Info.tsx
+++ 01 - Spaces-iOS/asifmohd/ui/src/components/Card/CardInfo/Info.tsx
@@ -1,18 +1,66 @@
-import React from "react"
+import React, { Props } from "react"
 import "./Info.scss"
-import { motion } from "framer-motion"
+import { motion, AnimatePresence } from "framer-motion"
+import { infoWrapperVariants } from "utils/variants"
+import { InfoType } from "model/interface"
 
-const Info = ({ title, description, style, ...animation }) => (
-  <motion.div {...animation}>
-    <h2 className="Card-title" style={style?.title}>
-      {title}
-    </h2>
-    <div className="Card-sub" style={style?.sub}>
-      {`${description?.count} ${description?.type}${
-        description?.count > 1 && "s"
-      }`}
-    </div>
-  </motion.div>
+const InfoWrapper = (props: Props<{}>) => (
+  <div className="Card-info-wrapper">
+    <motion.div
+      className="Card-info"
+      variants={infoWrapperVariants}
+      initial={"hidden"}
+      animate={"visible"}
+      exit={"hidden"}
+    >
+      {props.children}
+    </motion.div>
+  </div>
 )
+
+const TitleWrapper = (props: Props<{}>) => (
+  <div className="Card-desc-title">
+    <AnimatePresence exitBeforeEnter>{props.children}</AnimatePresence>
+  </div>
+)
+
+const Info: React.FC<InfoType> = ({
+  title,
+  description,
+  style,
+  isDescTitle = false,
+  thumbs = null,
+  animation,
+}) => {
+  const content = (
+    <>
+      <motion.div {...animation}>
+        <h2 className="Card-title" style={style?.title}>
+          {title}
+        </h2>
+        <div className="Card-sub" style={style?.sub}>
+          {`${description?.count} ${description?.type}${
+            description?.count > 1 && "s"
+          }`}
+        </div>
+      </motion.div>
+      {!!thumbs &&
+        thumbs.map(
+          (thumb, i) =>
+            i < 3 && (
+              <span className={"Card-thumb Card-thumb__" + i} key={i}>
+                <img src={thumb.image} alt={thumb.name} />
+              </span>
+            )
+        )}
+    </>
+  )
+
+  return !isDescTitle ? (
+    <InfoWrapper>{content}</InfoWrapper>
+  ) : (
+    <TitleWrapper>{content}</TitleWrapper>
+  )
+}
 
 export default Info
