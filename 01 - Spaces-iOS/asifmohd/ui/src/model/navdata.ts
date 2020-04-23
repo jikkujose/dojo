8386d4115e02835e640eafaea9686babd646838f
Asif Mohammed <asifm2882@gmail.com>
2020-04-23 18:30:03 +0530

    UI: move typeings to interface.ts

--- 01 - Spaces-iOS/asifmohd/ui/src/model/navdata.ts
+++ 01 - Spaces-iOS/asifmohd/ui/src/model/navdata.ts
@@ -4,20 +4,3 @@ export const navData = [
   { title: "boards", href: "#" },
   { title: "favorites", href: "#" },
 ]
-
-export interface Description {
-  type: string
-  count: number
-}
-export interface FaceThumb {
-  name: string
-  image: string
-}
-export interface spaceList {
-  id: string
-  title: string
-  description: Description
-  face_thumbs: FaceThumb[]
-  bg_image: string
-  icon: string
-}
