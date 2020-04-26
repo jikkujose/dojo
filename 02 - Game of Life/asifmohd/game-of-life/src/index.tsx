ab1d14994ff0572fb0c547fe1b41ed0af5323207
Asif Mohammed <asifm2882@gmail.com>
2020-04-26 14:27:37 +0530

    02-Life: remove unused code

--- 02 - Game of Life/asifmohd/game-of-life/src/index.tsx
+++ 02 - Game of Life/asifmohd/game-of-life/src/index.tsx
@@ -2,11 +2,6 @@ import React from "react"
 import ReactDOM from "react-dom"
 import "./index.css"
 import App from "./App"
-import * as serviceWorker from "./serviceWorker"
-
-const isMobileDevice = () =>
-  typeof window.orientation !== "undefined" ||
-  navigator.userAgent.indexOf("IEMobile") !== -1
 
 ReactDOM.render(
   <React.StrictMode>
@@ -14,18 +9,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 )
-
-if (isMobileDevice()) {
-  let vh = window.innerHeight * 0.01
-  document.documentElement.style.setProperty("--vh", `${vh}px`)
-
-  window.addEventListener("resize", () => {
-    let vh = window.innerHeight * 0.01
-    document.documentElement.style.setProperty("--vh", `${vh}px`)
-  })
-}
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister()
