473cf74d77d94574b7301af1136aa17aa6a26911
Asif Mohammed <asifm2882@gmail.com>
2020-04-26 14:25:57 +0530

    02-Life: add getCurrentGridDim Fn

--- 02 - Game of Life/asifmohd/game-of-life/src/utils/utils.ts
+++ 02 - Game of Life/asifmohd/game-of-life/src/utils/utils.ts
@@ -1,6 +1,10 @@
 export const gridGenerator = (row: number, col: number) =>
   Array.from(Array(row)).map(() => Array.from(Array(col)).fill(0))
 
+export const getCurrentGridDim = (grid: number[][]) => [
+  grid.length,
+  grid[0].length,
+]
 export const getWindowWidth = () =>
   window.innerWidth ||
   document.documentElement.clientWidth ||
