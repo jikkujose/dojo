9f36cf7dbacfdb9ed1cde4f78531f7850eef5880
Asif Mohammed <asifm2882@gmail.com>
2020-04-26 15:39:43 +0530

    02-Life: update reset button color

--- 02 - Game of Life/asifmohd/game-of-life/src/utils/config.ts
+++ 02 - Game of Life/asifmohd/game-of-life/src/utils/config.ts
@@ -17,7 +17,7 @@ export const config = {
       "The game is a zero-player game, meaning that its evolution is determined by its initial state, requiring no further input. One interacts with the Game of Life by creating an initial configuration and observing how it evolves. It is Turing complete and can simulate a universal constructor or any other Turing machine. Simply click on the grid to set intial state and click play!",
     startBtnColor: "#a3dc63",
     stopBtnColor: "#ff4343",
-    showSettingsColor: "#eeee",
+    showSettingsColor: "#fff",
     hideSettingsColor: "#ebd74b",
   },
 }
