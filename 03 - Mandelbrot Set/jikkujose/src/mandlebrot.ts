fdc08c1989c50e6935040fdc428eca5462175456
Jikku Jose <jikkujose@gmail.com>
2020-05-11 21:09:54 +0530

    03 | Make into single function

--- 03 - Mandelbrot Set/jikkujose/src/mandlebrot.ts
+++ 03 - Mandelbrot Set/jikkujose/src/mandlebrot.ts
@@ -1,16 +1,12 @@
 import { config } from "./config"
 
-export const mandlebrot = ([x, y], [cX, cY]) => {
-  return [x * x - y * y + cX, 2 * x * y + cY]
-}
-
 export const willConverge = ([cX, cY], n = config.iterationCount) => {
   let x = 0,
     y = 0,
     radiusSquare = 0
 
   for (let i = 0; i < n; i++) {
-    ;[x, y] = mandlebrot([x, y], [cX, cY])
+    ;[x, y] = [x * x - y * y + cX, 2 * x * y + cY]
     radiusSquare = x * x + y * y
 
     if (radiusSquare > 4) {
--- 03 - Mandelbrot Set/jikkujose/tests/mandlebrot.test.ts
+++ 03 - Mandelbrot Set/jikkujose/tests/mandlebrot.test.ts
@@ -1,32 +1,4 @@
-import { mandlebrot, willConverge } from "../src/mandlebrot"
-
-test("start with [0, 0] when c is [0, 0]", () => {
-  expect(mandlebrot([0, 0], [0, 0])).toEqual([0, 0])
-})
-
-test("start with [0, 0] when c is [1, 0]", () => {
-  expect(mandlebrot([0, 0], [1, 0])).toEqual([1, 0])
-})
-
-test("start with [0, 0] when c is [-1, 0]", () => {
-  expect(mandlebrot([0, 0], [-1, 0])).toEqual([-1, 0])
-})
-
-test("start with [-1, 0] when c is [-1, 0]", () => {
-  expect(mandlebrot([-1, 0], [-1, 0])).toEqual([0, 0])
-})
-
-test("start with [0, 0] when c is [10, 10]", () => {
-  expect(mandlebrot([0, 0], [10, 10])).toEqual([10, 10])
-})
-
-test("start with [1, 1] when c is [1, 1]", () => {
-  expect(mandlebrot([1, 1], [1, 1])).toEqual([1, 3])
-})
-
-test("start with [2, 3] when c is [4, 5]", () => {
-  expect(mandlebrot([2, 3], [4, 5])).toEqual([-1, 17])
-})
+import { willConverge } from "../src/mandlebrot"
 
 test("test for convergence for [-1, 0] @ 20th iteration", () => {
   expect(willConverge([-1, 0], 20)).toEqual(true)
